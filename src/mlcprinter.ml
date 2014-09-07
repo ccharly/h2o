dad79e955ac00015ce5327230c474b782db639d9
lisqlql <charly.n.chevalier@gmail.com>
2014-09-07 19:21:02 +0200

    Properly handles a_class and a_user_data

--- src/mlcprinter.ml
+++ src/mlcprinter.ml
@@ -27,14 +27,21 @@ module Map = Map.Make(struct type t = string let compare = compare end)
 
 let default_attr_builder = sprintf "a_%s %S"
 
+let a_class k v =
+    let v = Str.split (Str.regexp " ") v in
+    let v = List.fold_left (sprintf "%s%S; ") "" v in
+    sprintf "a_class [%s]" v
+
+let a_data k v =
+    let k = String.sub k 5 (String.length k) in
+    sprintf "a_user_data %S %S" k v
+
 let attr_specs =
     let s a = `string a in
     let r a = `regexp a in [
         (* Add specializations here *)
-        s "class", (fun _ v -> sprintf "a_class [%s]" v);
-        r "data-.+",
-            (fun k v ->
-                sprintf "a_user_data %S %S" k v)
+        s "class", a_class;
+        r "data-.+", a_data;
     ]
 
 (* smap = String map, rlist = Str.regexp list *)
