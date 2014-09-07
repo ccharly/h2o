1e36787bc2733cc789236c45fd0085e99c7bd344
lisqlql <charly.n.chevalier@gmail.com>
2014-09-07 18:05:02 +0200

    Add debug (print_token) + Handle files as args

--- src/mlc.ml
+++ src/mlc.ml
@@ -1,14 +1,34 @@
 open Printf
 
+let print_token = true
+
+let usage () =
+    eprintf "usage: %s <file1> <file2> ..\n" Sys.executable_name
+
 let () =
+    let argv = Array.sub Sys.argv 1 ((Array.length Sys.argv) - 1) in
+    let argc = Array.length argv in
+    if argc < 1 then
+        (usage (); exit 1);
     let open Mlcpar in
-    let file = "test/big.html" in
-    let in_ch = open_in file in
-    let lexbuf = Lexing.from_channel in_ch in
-    let eof = ref false in
-    while not !eof do
-        let token = Mlclex.html lexbuf in
-        Mlcprinter.print_token token;
-        if token = EOF then
-            eof := true
-    done
+    Array.iter
+      (fun f ->
+          printf "## %s\n" f;
+          let in_ch = open_in f in
+          let lexbuf = Lexing.from_channel in_ch in
+          let eof = ref false in
+          while not !eof do
+              if not print_token then begin
+                  let token = Mlcpar.doc Mlclex.html lexbuf in
+                  if token = `Eof then
+                      eof := true;
+                Mlcprinter.print_ast_node token;
+              end else begin
+                  let token = Mlclex.html lexbuf in
+                  if token = EOF then
+                      eof := true;
+                Mlcprinter.print_token token;
+              end
+          done;
+          printf "## (eof) %s\n" f;
+        ) argv
