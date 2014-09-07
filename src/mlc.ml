726d36372c4e37a94cc83496e9e3a61f639668f2
lisqlql <charly.n.chevalier@gmail.com>
2014-09-07 16:54:11 +0200

    Add Mlcprinter (pretty printer)

--- src/mlc.ml
+++ src/mlc.ml
@@ -1,36 +1,14 @@
-open Mlcpar
 open Printf
 
-let print_token t =
-    printf "::%d::" (Mlcloc.get_current_line ());
-    match t with
-    | DocType d ->
-            printf "DocType:%s\n" d
-    | Data d ->
-            printf "Data:%s\n" d
-    | Tag (n,a) ->
-            printf "Tag:%s\n" n;
-            List.iter (fun (a,b) -> printf "attr:%s=%s\n" a b) a
-    | TagStart (n,a) ->
-            printf "TagStart:%s\n" n;
-            List.iter (fun (a,b) -> printf "attr:%s=%s\n" a b) a
-    | TagEnd (n) ->
-            printf "TagEnd:%s\n" n
-    | Comment (c) ->
-            printf "Comment:%s\n" c
-    | EOF ->
-            printf "EOF\n"
-    | _ ->
-            failwith "print_token: unknown token."
-
 let () =
+    let open Mlcpar in
     let file = "test/big.html" in
     let in_ch = open_in file in
     let lexbuf = Lexing.from_channel in_ch in
     let eof = ref false in
     while not !eof do
         let token = Mlclex.html lexbuf in
-        print_token token;
+        Mlcprinter.print_token token;
         if token = EOF then
             eof := true
-    done;
+    done
---
+++ src/mlcprinter.ml
@@ -0,0 +1,25 @@
+open Printf
+
+let print_token t =
+    let open Mlcpar in
+    printf "::%d::" (Mlcloc.get_current_line ());
+    match t with
+    | DocType d ->
+            printf "DocType:%s\n" d
+    | Data d ->
+            printf "Data:%s\n" d
+    | Tag (n,a) ->
+            printf "Tag:%s\n" n;
+            List.iter (fun (a,b) -> printf "attr:%s=%s\n" a b) a
+    | TagStart (n,a) ->
+            printf "TagStart:%s\n" n;
+            List.iter (fun (a,b) -> printf "attr:%s=%s\n" a b) a
+    | TagEnd (n) ->
+            printf "TagEnd:%s\n" n
+    | Comment (c) ->
+            printf "Comment:%s\n" c
+    | EOF ->
+            printf "EOF\n"
+    | _ ->
+            failwith "print_token: unknown token."
+
