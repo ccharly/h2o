b99c21310ce0e32c8eac40d251c53a57d18a68e9
lisqlql <charly.n.chevalier@gmail.com>
2014-09-14 14:14:26 +0200

    Ignore attr by default + Add all default attrs
    These default attrs come from tyxml

--- src/output/h2o_attr_printer.ml
+++ src/output/h2o_attr_printer.ml
@@ -45,7 +45,8 @@ end
 class attr : attr_t = object(this)
     method matches _ = true
 
-    method ignore = false
+    (* Ignore every attributes that we don't know *)
+    method ignore = true
 
     method value _ v =
         sprintf "%S" v
@@ -69,6 +70,7 @@ end
 class attr_default = object
     inherit attr
 
+    method ignore = false
     method register = false
 end
 
@@ -77,6 +79,7 @@ class attr_string n = object
 
     val n = n
 
+    method ignore = false
     method matches name =
         (n = name)
 end
@@ -86,20 +89,218 @@ class attr_regexp r = object
 
     val reg = Str.regexp r
 
+    method ignore = false
     method matches name =
         Str.string_match reg name 0
 end
 
 let () =
-    (* Register attributes to be ignored *)
-    (* role *)
-    ignore (object
-        inherit attr_string "role"
-
-        method ignore = true
-    end);
+    (* Register all default attributes extracted from tyxml *)
+    let make_attr n =
+      ignore (new attr_string n)
+    in
+    List.iter make_attr [
+      "autocomplete";
+      "async";
+      "autofocus";
+      "autoplay";
+      "challenge";
+      "contenteditable";
+      "contextmenu";
+      "controls";
+      "dir";
+      "draggable";
+      "form";
+      "formaction";
+      "formenctype";
+      "formmethod";
+      "formnovalidate";
+      "formtarget";
+      "hidden";
+      "high";
+      "icon";
+      "ismap";
+      "keytype";
+      "list";
+      "loop";
+      "low";
+      "max";
+      "input_max";
+      "min";
+      "input_min";
+      "novalidate";
+      "open";
+      "optimum";
+      "pattern";
+      "placeholder";
+      "poster";
+      "preload";
+      "pubdate";
+      "radiogroup";
+      "required";
+      "reversed";
+      "sandbox";
+      "spellcheck";
+      "scoped";
+      "seamless";
+      "sizes";
+      "span";
+      "srcdoc*)";
+      "srclang";
+      "start";
+      "step";
+      "wrap";
+      (*"class";*)
+      (*"user_data";*)
+      "id";
+      "title";
+      (*"xml_lang";*)
+      "onabort";
+      "onafterprint";
+      "onbeforeprint";
+      "onbeforeunload";
+      "onblur";
+      "oncanplay";
+      "oncanplaythrough";
+      "onchange";
+      "onclick";
+      "oncontextmenu";
+      "ondblclick";
+      "ondrag";
+      "ondragend";
+      "ondragenter";
+      "ondragleave";
+      "ondragover";
+      "ondragstart";
+      "ondrop";
+      "ondurationchange";
+      "onemptied";
+      "onended";
+      "onerror";
+      "onfocus";
+      "onformchange";
+      "onforminput";
+      "onhashchange";
+      "oninput";
+      "oninvalid";
+      "onmousedown";
+      "onmouseup";
+      "onmouseover";
+      "onmousemove";
+      "onmouseout";
+      "onmousewheel";
+      "onoffline";
+      "ononline";
+      "onpause";
+      "onplay";
+      "onplaying";
+      "onpagehide";
+      "onpageshow";
+      "onpopstate";
+      "onprogress";
+      "onratechange";
+      "onreadystatechange";
+      "onredo";
+      "onresize";
+      "onscroll";
+      "onseeked";
+      "onseeking";
+      "onselect";
+      "onshow";
+      "onstalled";
+      "onstorage";
+      "onsubmit";
+      "onsuspend";
+      "ontimeupdate";
+      "onundo";
+      "onunload";
+      "onvolumechange";
+      "onwaiting";
+      "onkeypress";
+      "onkeydown";
+      "onkeyup";
+      "onload";
+      "onloadeddata";
+      "onloadedmetadata";
+      "onloadstart";
+      "onmessage";
+      "version";
+      "xmlns";
+      "manifest";
+      "cite";
+      "xml_space";
+      "accesskey";
+      "charset";
+      "accept_charset";
+      "accept";
+      "href";
+      "hreflang";
+      "rel";
+      "tabindex";
+      "mime_type";
+      "datetime";
+      "action";
+      "checked";
+      "cols";
+      "enctype";
+      "for";
+      "for_list";
+      "maxlength";
+      "method";
+      "multiple";
+      "name";
+      "rows";
+      "selected";
+      "size";
+      "src";
+      "input_type";
+      "text_value";
+      "int_value";
+      "value";
+      "float_value";
+      "disabled";
+      "readonly";
+      "button_type";
+      "command_type";
+      "menu_type";
+      "label";
+      "align";
+      "axis";
+      "colspan";
+      "headers";
+      "rowspan";
+      "scope";
+      "summary";
+      "border";
+      "cellpadding";
+      "cellspacing";
+      "datapagesize";
+      "rules";
+      "char";
+      "charoff";
+      "alt";
+      "height";
+      "width";
+      "shape";
+      "coords";
+      "usemap";
+      "data";
+      "codetype";
+      "fs_rows";
+      "fs_cols";
+      "frameborder";
+      "marginheight";
+      "marginwidth";
+      "scrolling";
+      "target";
+      "content";
+      "http_equiv";
+      "defer";
+      "media";
+      "style";
+      "property";
+    ]
     (* End of register *)
-    ()
 
 let () =
     (* Register all *)
